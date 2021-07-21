bb7f09bb20963f7a70ef8d21c1e594bf13582414
Jonathan Pappas <jonnyboypappas@gmail.com>
2021-07-21 11:33:42 -0700

    Draggadon

--- SpriteKitIntro/SpriteKitIntro/GameScene.swift
+++ SpriteKitIntro/SpriteKitIntro/GameScene.swift
@@ -5,8 +5,19 @@
 //  Created by Jonathan Pappas on 7/21/21.
 //
 
+import Foundation
 import SpriteKit
 import GameplayKit
+
+let M_PI_2_f = Float(Double.pi / 2)
+let M_PI_f = Float(Double.pi)
+public func sinFloat(_ num:Float)->Float {
+    return sin(num)
+}
+public func SineEaseOut(_ p:Float)->Float {
+    return sinFloat(p * M_PI_2_f)
+}
+
 
 
 extension CGPoint {
@@ -31,7 +42,7 @@ class GameScene: SKScene {
         node3.setScale(0.5)
         let node4 = SKSpriteNode(color: .gray, size: .hundred)
         
-        h = HStack(nodes: [node1, node2, node3, node4])
+        h = HStack(nodes: [node1.padding, node2.padding, node3.padding, node4.padding])
         addChild(h)
         keepInsideScene(h)
         h.centerOn(self)
@@ -44,11 +55,25 @@ class GameScene: SKScene {
         node.setScale(min((size.width / nodeSize.width) * node.xScale, (size.height / nodeSize.height) * node.yScale))
     }
 
+    var (previousX, previousY): (CGFloat, CGFloat) = (0, 0)
+    override func mouseDragged(with event: NSEvent) {
+        h.run(.moveBy(x: event.deltaX, y: -event.deltaY, duration: 0.1))
+        (previousX, previousY) = (event.deltaX, event.deltaY)
+        //print(event.deltaX)
+    }
+    override func mouseUp(with event: NSEvent) {
+        let uwu = SKAction.moveBy(x: previousX*20, y: -previousY*20, duration: 0.5)
+        uwu.timingFunction = SineEaseOut(_:)
+        h.run(uwu)
+    }
+    
     override func mouseDown(with event: NSEvent) {
-        let foo: [SKSpriteNode] = (1...h.children.count).map { _ in SKSpriteNode(color: .gray, size: .hundred) }
+        (previousX, previousY) = (0,0)
+        let foo: [SKNode] = (1...h.children.count).map { _ in SKSpriteNode(color: .gray, size: .hundred).padding }
         h.append(node: VStack.init(nodes: foo))
         //h.append(node: h.copied)
         //h.append(nodes: h.copiedChildren)
+        
         keepInsideScene(h)
         h.centerOn(self)
         //h.centerAt(point: .midScreen)
@@ -59,7 +84,23 @@ class GameScene: SKScene {
 
 
 
+extension CGSize {
+    func padding(_ with: CGFloat) -> Self {
+        return .init(width: self.width + with, height: self.height + with)
+    }
+}
+
 extension SKNode {
+    var padding: SKNode {
+        let oldScale = (self.xScale, self.yScale)
+        setScale(1)
+        let newNode = SKSpriteNode.init(color: .white, size: self.calculateAccumulatedFrame().size.padding(20))
+        addChild(newNode)
+        (xScale, yScale) = oldScale
+        newNode.alpha = 0
+        return self
+    }
+    
     var copied: SKNode {
         return self.copy() as! SKNode
     }
