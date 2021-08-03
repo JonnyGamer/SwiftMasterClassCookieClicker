3b128194ded69b7868bdce272bdf1e51ed43b1a7
Jonathan Pappas <jonnyboypappas@gmail.com>
2021-08-03 15:14:00 -0700

    huh

--- PhysicsTesting/EverMazeSceneHost.swift
+++ PhysicsTesting/EverMazeSceneHost.swift
@@ -35,63 +35,63 @@ class EverMazeSceneHost: HostingScene {
     }
 }
 
-// Extra Extensions
-extension Array where Element == Int {
-    func next() -> Self {
-        return map { $0 + 1 }
-    }
-}
-extension NewEverMaze {
-    static func regularPuzzle(_ someSize: [Int]) -> NewEverMaze {
-        let xrand = Int.random(in: 0...(someSize[0]-1))
-        let yrand = Int.random(in: 0...(someSize[1]-1))
-        
-        let uwu = NewEverMaze.init(
-            someSize,
-            [.init(covers: [[0,0]], position: [xrand, yrand])],
-            randomWalls: { .random() && .random() }
-        )
-        uwu.makeMaze()
-        
-        if (uwu.end?.movements.count ?? 0) < min(someSize[0], someSize[1]) {
-            return regularPuzzle(someSize)
-        }
-        
-        return uwu
-    }
-    
-    static func nPlayerPuzzle(players: Int,_ someSize: [Int]) -> NewEverMaze {
-        var rands: Set<MovableDots> = []
-        this: for _ in 1...players {
-            if !rands.isEmpty {
-                for _ in 1...100 {
-                    let xrand = Int.random(in: 0...(someSize[0]-1))
-                    let yrand = Int.random(in: 0...(someSize[1]-1))
-                    let poso = Pos([xrand, yrand])
-                    if !rands.contains(where: { $0.position == poso }) {
-                        rands.insert(.init(covers: [[0,0]], position: poso))
-                        continue this
-                    }
-                }
-                fatalError("Not enough spaces bro...")
-            } else {
-                let xrand = Int.random(in: 0...(someSize[0]-1))
-                let yrand = Int.random(in: 0...(someSize[1]-1))
-                rands.insert(.init(covers: [[0,0]], position: [xrand, yrand]))
-            }
-        }
-        
-        let uwu = NewEverMaze.init(
-            someSize,
-            rands,
-            randomWalls: { .random() && .random() }
-        )
-        uwu.makeMaze()
-        
-        if (uwu.end?.movements.count ?? 0) < min(someSize[0], someSize[1]) {
-            return nPlayerPuzzle(players: players, someSize)
-        }
-        
-        return uwu
-    }
-}
+//// Extra Extensions
+//extension Array where Element == Int {
+//    func next() -> Self {
+//        return map { $0 + 1 }
+//    }
+//}
+//extension NewEverMaze {
+//    static func regularPuzzle(_ someSize: [Int]) -> NewEverMaze {
+//        let xrand = Int.random(in: 0...(someSize[0]-1))
+//        let yrand = Int.random(in: 0...(someSize[1]-1))
+//
+//        let uwu = NewEverMaze.init(
+//            someSize,
+//            [.init(covers: [[0,0]], position: [xrand, yrand])],
+//            randomWalls: { .random() && .random() }
+//        )
+//        uwu.makeMaze()
+//
+//        if (uwu.end?.movements.count ?? 0) < min(someSize[0], someSize[1]) {
+//            return regularPuzzle(someSize)
+//        }
+//
+//        return uwu
+//    }
+//
+//    static func nPlayerPuzzle(players: Int,_ someSize: [Int]) -> NewEverMaze {
+//        var rands: Set<MovableDots> = []
+//        this: for _ in 1...players {
+//            if !rands.isEmpty {
+//                for _ in 1...100 {
+//                    let xrand = Int.random(in: 0...(someSize[0]-1))
+//                    let yrand = Int.random(in: 0...(someSize[1]-1))
+//                    let poso = Pos([xrand, yrand])
+//                    if !rands.contains(where: { $0.position == poso }) {
+//                        rands.insert(.init(covers: [[0,0]], position: poso))
+//                        continue this
+//                    }
+//                }
+//                fatalError("Not enough spaces bro...")
+//            } else {
+//                let xrand = Int.random(in: 0...(someSize[0]-1))
+//                let yrand = Int.random(in: 0...(someSize[1]-1))
+//                rands.insert(.init(covers: [[0,0]], position: [xrand, yrand]))
+//            }
+//        }
+//
+//        let uwu = NewEverMaze.init(
+//            someSize,
+//            rands,
+//            randomWalls: { .random() && .random() }
+//        )
+//        uwu.makeMaze()
+//
+//        if (uwu.end?.movements.count ?? 0) < min(someSize[0], someSize[1]) {
+//            return nPlayerPuzzle(players: players, someSize)
+//        }
+//
+//        return uwu
+//    }
+//}
